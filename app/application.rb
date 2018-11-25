315a8a8aa4b3c3aebfdc80ec4cc9041e00903b41
Greg Morgan <gregm9999@gmail.com>
2018-11-25 20:55:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,10 @@
 class Greeter
-  if Time.now.hour<12
-    resp.write "Good Morning!"
-  else
-    resp.write "Good Afternoon!"
+  def call(env)
+    resp = Rack::Response.new
+    if Time.now.hour<12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
   end
 end
